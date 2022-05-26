72bd97a0820cfa8829fc620862a0947563b0c9ea
joshuaramat <87837577+joshuaramat@users.noreply.github.com>
2022-05-25 21:23:53 -0700

    added about component

---
+++ src/components/about.js
@@ -0,0 +1,15 @@
+function About() {
+    return (
+        <>
+            <section className="about">
+                <div className="content">
+                    <h4>About</h4>
+                    <img src="" alt="" className="photo" />
+                    <p></p>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default About;
