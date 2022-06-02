02eafb4425e74a845afcbd67e7926fa5fbe07c9d
Joshua Ramat <87837577+joshuaramat@users.noreply.github.com>
2022-06-02 16:25:30 -0700

    updated Nav component to reflect anchor tags and added project links to portfolio component for testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./home";
 import About from "./components/About/about";
 import Nav from "./components/Nav/nav";
 import Port from "./components/Portfolio/port";
@@ -10,8 +11,8 @@ function App() {
       <Nav />
       <main>
         <Routes>
-          <Route path="/" element={<About/>} />
-          <Route path="/about" element={<About/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/portfolio" element={<Port />} />
         </Routes>
       </main>
