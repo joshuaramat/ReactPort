55c8d7505f4063df75155b88810ab5ebd24ed93f
Joshua Ramat <87837577+joshuaramat@users.noreply.github.com>
2022-06-06 12:45:30 -0700

    added form element to submit contact info

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -2,7 +2,46 @@ import { React, useState } from "react";
 
 function Contact() {
   return (
-    <p>Contact Form</p>
+    <section className='contact cover'>
+      <div className='content'>
+        <h4>Contact</h4>
+        <form id='contact-form' onSubmit={}>
+          <div className='form-group'>
+            <label htmlFor='name'>Name</label>
+            <input 
+              type='text' 
+              className='form-control' 
+              placeholder='name' 
+              defaultValue={} 
+              onBlur={} 
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='email'>E-Mail</label>
+            <input 
+              type='text' 
+              className='form-control' 
+              placeholder='email' 
+              defaultValue={} 
+              onBlur={} 
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='Message'>Message</label>
+            <input 
+              type='text' 
+              className='form-control' 
+              placeholder='message' 
+              defaultValue={} 
+              onBlur={} 
+            />
+          </div>
+          <button type='submit' className='btn'>Submit</button>
+        </form>
+      </div>
+    </section>
   );
 }
 
