6f967711ad057f2d3cf1913977752ea85d841496
Joshua Ramat <87837577+joshuaramat@users.noreply.github.com>
2022-06-06 18:00:07 -0700

    undid react fragments and conditional rendering in App.js

--- src/App.js
+++ src/App.js
@@ -13,15 +13,10 @@ function App() {
       <Nav />
       <main>
         <Routes>
-          {!contactSelected ? (
-            <>
-              <Route path='/' element={<About />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/portfolio' element={<Port />} />
-            </>
-            ) : (
-              <Route path='/contact' element={<Contact />} />
-          )}
+            <Route path='/' element={<About />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/portfolio' element={<Port />} />
+            <Route path='/contact' element={<Contact />} />
         </Routes>
       </main>
       <footer />
