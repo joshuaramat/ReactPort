55ba706df63138d6b665e4eb4324266471424d3d
Joshua Ramat <87837577+joshuaramat@users.noreply.github.com>
2022-06-06 17:57:25 -0700

    added conditional rendering to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './components/About/about';
 import Nav from './components/Nav/nav';
@@ -6,15 +6,22 @@ import Port from './components/Portfolio/port';
 import Contact from './components/Contact/contact.js';
 
 function App() {
+  const [ contactSelected, setContactSelected] = useState(false);
+
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Nav />
       <main>
         <Routes>
-          <Route path='/' element={<About />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/portfolio' element={<Port />} />
-          <Route path='/contact' element={<Contact />} />
+          {!contactSelected ? (
+            <>
+              <Route path='/' element={<About />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/portfolio' element={<Port />} />
+            </>
+            ) : (
+              <Route path='/contact' element={<Contact />} />
+          )}
         </Routes>
       </main>
       <footer />
