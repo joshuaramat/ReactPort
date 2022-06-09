61f651ef9fa9abb28360ab0a8b6e474ae57da61f
joshuaramat <87837577+joshuaramat@users.noreply.github.com>
2022-06-09 11:36:22 -0700

    homepage now works

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -5,7 +5,7 @@ function Nav() {
 	return (
 		<header>
 			<h2>
-				<a data-testid='link' href='/about' className='title'>
+				<a data-testid='link' href='/port-react' className='title'>
 					Joshua Ramat
 				</a>
 			</h2>
