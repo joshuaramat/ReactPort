fbf0844ecc77b4a2af78aeeb20ddcc47ed19341b
joshuaramat <87837577+joshuaramat@users.noreply.github.com>
2022-06-10 23:50:56 -0700

    added bio

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -7,8 +7,13 @@ function About() {
 			<section className='about cover'>
 				<div className='content'>
 					<h4>About</h4>
-					<img src={joshua} alt='' className='photo' />
-					<p>test bio</p>
+					<img src={joshua} alt='portrait of Joshua' className='photo' />
+					<p>Hello, My name is Joshua! I have a brackground as a physical therapy aide with a mission to help others. I made the switch to web dev when I realized my affinity for technology.
+
+					<br></br>
+					<br></br>
+
+					Thorogh and meticulous software engineer with the ability to analyze user needs and design software solutions customized to clients preferences. Oustanding attention to detail with a passion for problem-solving and continued learning. Applying my background in physical therapy, I haves trong written and people skills with experience collaborating in groups. Proficient in HTML/CSS, JavaScript, jQuery, Node.js, React.js, MySQL, Heroku, and Git Workflow. Come checkout my work!</p>
 				</div>
 			</section>
 		</>
