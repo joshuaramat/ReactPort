23a569a327b84f5fd6cef1b352d5f267a6d0419b
joshuaramat <87837577+joshuaramat@users.noreply.github.com>
2022-06-11 23:38:48 -0700

    added display: grid to portfolio component and added resume and skills section

--- src/components/Portfolio/port.js
+++ src/components/Portfolio/port.js
@@ -2,14 +2,17 @@ import React from 'react';
 import moviedashboard from '../../assets/movieDashboard.jpeg';
 import codequiz from '../../assets/code-quiz.png';
 import notetaker from '../../assets/note-taker.png';
+import passwordgen from '../../assets/password-generator.jpg'
+import weatherdash from '../../assets/weather-dashboard.png'
+import scheduler from '../../assets/work-day-scheduler.png'
 
 function Port() {
 	return (
 		<section className='port cover'>
 			<div className='content'>
 				<h4>Portfolio</h4>
-				<ul>
-					<li>
+				<ul className='grid'>
+					<li className='grid-item'>
 						<a href='https://github.com/joshuaramat/movie-dashboard' class='github' target='_blank' rel='noreferrer'>
 							<i className='fab fa-github'></i>
 						</a>
@@ -30,7 +33,7 @@ function Port() {
 						</a>
 					</li>
 
-					<li>
+					<li className='grid-item'>
 						<a href='https://github.com/joshuaramat/code-quiz' class='github' target='_blank' rel='noreferrer'>
 							<i className='fab fa-github'></i>
 						</a>
@@ -43,8 +46,8 @@ function Port() {
 						</a>
 					</li>
 
-					<li>
-            <a href="https://github.com/tw2159/Note-Taker" class="github" target="_blank" rel="noreferrer">
+					<li className='grid-item'>
+            <a href="https://github.com/joshuaramat/note-taker" class="github" target="_blank" rel="noreferrer">
 							<i className="fab fa-github"></i>
 						</a>
             <a href="https://limitless-dusk-41992.herokuapp.com/" target="_blank" rel="noreferrer">
@@ -52,6 +55,45 @@ function Port() {
               <div className="project-info">
                 <span>Note Taker</span>
                 <span className="project-details">Express.js</span>
+              </div>
+            </a>
+          </li>
+
+					<li className='grid-item'>
+            <a href="https://github.com/joshuaramat/password-gen" class="github" target="_blank" rel="noreferrer">
+							<i className="fab fa-github"></i>
+						</a>
+            <a href="https://joshuaramat.github.io/password-gen/" target="_blank" rel="noreferrer">
+              <img src={passwordgen} alt="" className="screenshot" />
+              <div className="project-info">
+                <span>Password Generator</span>
+                <span className="project-details">JavaScript</span>
+              </div>
+            </a>
+          </li>
+
+					<li className='grid-item'>
+            <a href="https://github.com/joshuaramat/weather-dashboard" class="github" target="_blank" rel="noreferrer">
+							<i className="fab fa-github"></i>
+						</a>
+            <a href="https://joshuaramat.github.io/weather-dashboard/" target="_blank" rel="noreferrer">
+              <img src={weatherdash} alt="" className="screenshot" />
+              <div className="project-info">
+                <span>Weather Dashboard</span>
+                <span className="project-details">API Development</span>
+              </div>
+            </a>
+          </li>
+
+					<li className='grid-item'>
+            <a href="https://github.com/joshuaramat/scheduler" class="github" target="_blank" rel="noreferrer">
+							<i className="fab fa-github"></i>
+						</a>
+            <a href="https://joshuaramat.github.io/scheduler/" target="_blank" rel="noreferrer">
+              <img src={scheduler} alt="" className="screenshot" />
+              <div className="project-info">
+                <span>Workday Scheduler</span>
+                <span className="project-details">JavaScript</span>
               </div>
             </a>
           </li>
